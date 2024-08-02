91fb2cb61228d4b34f0bddd2af5dbbafaaec8fd7
sanghun <nrhys2015@danbiedu.co.kr>
2024-08-02 15:39:01 +0900

    fix: setup add classifiers

--- setup.py
+++ setup.py
@@ -17,6 +17,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
     ],
     python_requires='>=3.6',
 )
