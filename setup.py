03bde01f66efc6ae7d329060ff40d746f52a5f81
sanghun <nrhys2015@danbiedu.co.kr>
2024-08-02 16:06:49 +0900

    fix: Add find_pakages

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="method-versioning",
-    version="0.0.1",
+    version="0.0.2",
     author="Sanghun Lee",
     author_email="nrhys2005@gmail.com",
     description="Method versioning package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nrhys2005/method-versioning",
-    packages=setuptools.find_packages(),
+    packages=find_packages(include=['method_versioning', 'method_versioning.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
